443ffe3b7ccab08390548832baab633301a1296f
Victor Lucio <victorl@determination.determination>
2020-07-02 11:29:14 -0300

    Finalização do GET para Download, Adição do Cors e Outros Detalhes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const connection = require('./database/connection');
+const pathAbsolute = require('path');
 
 const router = express.Router();
 
-router.get('/download/:id', (req, res)=>{
+router.get('/download/:id', async (req, res)=>{
     const id = req.params.id;
-    console.log(id);
-    res.end();
+    try{
+        const { path, type } = (await connection.select('path', 'type').from('library').where('id', '=', id).first());
+        const fileURL = pathAbsolute.resolve(__dirname, '../', path + '.' + type);
+        return res.sendFile(fileURL);
+    }catch(err){
+        console.log(err);
+        return res.send('Ocorreu algum erro.');
+    }
+    //return res.json({id});
+})
+
+router.get('/library', async (req, res)=>{
+    const library = await connection.select('*').from('library');
+    return res.json(library);
 })
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = 8000;
 
 const Routes = require('./routes');
 
+app.use(cors);
 app.use('/', Routes);
 
 app.listen(port);
